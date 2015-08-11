c8208ef9c48ad4bc0a71db84e840642ce25b820a
Henrik Frystyk Nielsen <henrikn@microsoft.com>
2015-08-11 12:15:01 -0700

    Tweaked support for VS 2013 (didn't look for msbuild in the right place)

--- build.cmd
+++ build.cmd
@@ -11,7 +11,7 @@ REM Find the most recent 32bit MSBuild.exe on the system. Require v12.0 (install
 REM is not supported. Also handle x86 operating systems, where %ProgramFiles(x86)% is not defined. Always quote the
 REM %MSBuild% value when setting the variable and never quote %MSBuild% references.
 set MSBuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
-if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 if not exist %MSBuild% (
   echo Could not find msbuild.exe. Please run this from a Visual Studio developer prompt
   goto BuildFail
