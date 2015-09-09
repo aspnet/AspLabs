00b5cf2fa166c2b28a6b5f6e5ffa09646c672b6d
Henrik Frystyk Nielsen <henrikn@microsoft.com>
2015-09-09 07:52:18 -0700

    Removed previous workaround.

--- src/Microsoft.AspNet.WebHooks.Receivers.Salesforce/WebHooks/SalesforceSoapWebHookReceiver.cs
+++ src/Microsoft.AspNet.WebHooks.Receivers.Salesforce/WebHooks/SalesforceSoapWebHookReceiver.cs
@@ -57,7 +57,7 @@ namespace Microsoft.AspNet.WebHooks
                 EnsureSecureConnection(request);
 
                 // Read the request entity body
-                XElement data = await this.ReadBodyAsXmlAsync(request);
+                XElement data = await ReadAsXmlAsync(request);
                 SalesforceNotifications notifications = new SalesforceNotifications(data);
 
                 // Ensure that the organization ID matches the expected value.
