6d8dab2d8e58c898fbc53557f0050d1c186bfcde
John Luo <jt.luo@mail.utoronto.ca>
2015-12-23 15:38:52 -0800

    Adding back middleware initialization with options instance.

--- src/Microsoft.AspNet.Proxy/ProxyExtension.cs
+++ src/Microsoft.AspNet.Proxy/ProxyExtension.cs
@@ -30,5 +30,25 @@ namespace Microsoft.AspNet.Builder
 
             return app.UseMiddleware<ProxyMiddleware>(options);
         }
+
+        /// <summary>
+        /// Sends request to remote server as specified in options
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="options">Options for setting port, host, and scheme</param>
+        /// <returns></returns>
+        public static IApplicationBuilder RunProxy(this IApplicationBuilder app, ProxyOptions options)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            return app.UseMiddleware<ProxyMiddleware>(options);
+        }
     }
 }
