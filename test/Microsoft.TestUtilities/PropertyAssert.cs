3e619a5c5b2cc4084d6b03f4f236b109f7471fdb
Henrik Frystyk Nielsen <henrikn@microsoft.com>
2016-02-11 16:01:15 -0800

    Fixed file header.

--- test/Microsoft.TestUtilities/PropertyAssert.cs
+++ test/Microsoft.TestUtilities/PropertyAssert.cs
@@ -1,6 +1,5 @@
-﻿// ---------------------------------------------------------------------------- 
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// ----------------------------------------------------------------------------
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
 using System.Collections;
