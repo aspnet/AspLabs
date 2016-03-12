25c8498eccdaa0e9454f2b316939def58123ae46
Henrik Frystyk Nielsen <henrikn@microsoft.com>
2016-03-11 16:30:39 -0800

    Added dropbox sample

---
+++ samples/DropboxReceiver/WebHooks/DropboxWebHookHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace DropboxReceiver.WebHooks
+{
+    public class DropboxWebHookHandler : WebHookHandler
+    {
+        public DropboxWebHookHandler()
+        {
+            this.Receiver = DropboxWebHookReceiver.ReceiverName;
+        }
+
+        public override Task ExecuteAsync(string generator, WebHookHandlerContext context)
+        {
+            // For more information about Dropbox WebHook payloads, please see 
+            // 'https://www.dropbox.com/developers/reference/webhooks'
+            JObject entry = context.GetDataOrDefault<JObject>();
+
+            return Task.FromResult(true);
+        }
+    }
+}
