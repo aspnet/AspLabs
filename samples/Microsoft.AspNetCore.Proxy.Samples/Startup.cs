e84497a25874847c30c383df4295f39d6ad10acd
John Luo <jt.luo@mail.utoronto.ca>
2016-03-22 12:05:04 -0700

    React to Hosting changes

--- samples/Microsoft.AspNetCore.Proxy.Samples/Startup.cs
+++ samples/Microsoft.AspNetCore.Proxy.Samples/Startup.cs
@@ -24,7 +24,7 @@ namespace Microsoft.AspNetCore.Proxy
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseDefaultConfiguration(args)
+                .UseDefaultHostingConfiguration(args)
                 .UseServer("Microsoft.AspNetCore.Server.Kestrel")
                 .UseIISPlatformHandlerUrl()
                 .UseStartup<Startup>()
