33f36c8a82407e56ed134ce0466b9a152ef675e7
Henrik Frystyk Nielsen <henrikn@microsoft.com>
2016-04-14 08:09:58 -0700

    Added GitHub sample

---
+++ samples/GitHubReceiver/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace GitHubReceiver
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Initialize GitHub WebHook receiver
+            config.InitializeReceiveGitHubWebHooks();
+        }
+    }
+}
