aspnet/WebHooks

Added support for Zendesk

samples/ZendeskReceiver/App_Start/WebApiConfig.cs
@@ -1,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace ZendeskReceiver
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Initialize ZendeskReceiver WebHook receiver
+            config.InitializeReceiveBitbucketWebHooks();
+        }
+    }
+}

src/Microsoft.AspNet.WebHooks.Receivers.Zendesk/Extensions/HttpConfigurationExtensions.cs
@@ -1,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using Microsoft.AspNet.WebHooks.Config;
+
+namespace System.Web.Http
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpConfiguration"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class HttpConfigurationExtensions
+    {
+        /// <summary>
+        /// Initializes support for receiving Zendesk WebHooks.
+        /// A sample WebHook URI is '<c>https://&lt;host&gt;/api/webhooks/incoming/zendesk/{id}?code=83699ec7c1d794c0c780e49a5c72972590571fd8</c>'.
+        /// For security reasons the WebHook URI must be an <c>https</c> URI and contain a 'code' query parameter with the
+        /// same value as configured in the '<c>MS_WebHookReceiverSecret_Bitbucket</c>' application setting.
+        /// The 'code' parameter must be between 32 and 128 characters long.
+        /// For details about Zendesk WebHooks, see <c>https://developer.zendesk.com/embeddables/docs/ios/push_notifications_webhook#webhook-api</c>.
+        /// For complete details about Zendesk APIs, see <c>https://developer.zendesk.com/rest_api/docs/core/introduction</c>.
+        /// </summary>
+        /// <param name="config">The current <see cref="HttpConfiguration"/>config.</param>
+        public static void InitializeReceiveBitbucketWebHooks(this HttpConfiguration config)
+        {
+            WebHooksConfig.Initialize(config);
+        }
+    }
+}

src/Microsoft.AspNet.WebHooks.Receivers.Zendesk/WebHooks/ZendeskDevice.cs
@@ -1,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNet.WebHooks
+{
+    /// <summary>
+    /// Contains the information about registered device for Zendesk (ios, android)
+    /// </summary>
+    public class ZendeskDevice
+    {
+        /// <summary>
+        /// The device identifier/token that was registered through the SDK
+        /// </summary>
+        [JsonProperty("identifier")]
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// The device type. Possible values: "ios" or "android"
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}

aspnet/AspLabs

samples/ZendeskReceiver/App_Start/WebApiConfig.cs
@@ -1,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace ZendeskReceiver
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Initialize ZendeskReceiver WebHook receiver
+            config.InitializeReceiveZendeskWebHooks();
+        }
+    }
+}

src/Microsoft.AspNet.WebHooks.Receivers.Zendesk/Extensions/HttpConfigurationExtensions.cs
@@ -1,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using Microsoft.AspNet.WebHooks.Config;
+
+namespace System.Web.Http
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpConfiguration"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class HttpConfigurationExtensions
+    {
+        /// <summary>
+        /// Initializes support for receiving Zendesk WebHooks.
+        /// A sample WebHook URI is '<c>https://&lt;host&gt;/api/webhooks/incoming/zendesk/{id}?code=83699ec7c1d794c0c780e49a5c72972590571fd8</c>'.
+        /// For security reasons the WebHook URI must be an <c>https</c> URI and contain a 'code' query parameter with the
+        /// same value as configured in the '<c>MS_WebHookReceiverSecret_Zendesk</c>' application setting.
+        /// The 'code' parameter must be between 32 and 128 characters long.
+        /// For details about Zendesk WebHooks, see <c>https://developer.zendesk.com/embeddables/docs/ios/push_notifications_webhook</c>.
+        /// For complete details about Zendesk APIs, see <c>https://developer.zendesk.com/rest_api/docs/core/introduction</c>.
+        /// </summary>
+        /// <param name="config">The current <see cref="HttpConfiguration"/>config.</param>
+        public static void InitializeReceiveZendeskWebHooks(this HttpConfiguration config)
+        {
+            WebHooksConfig.Initialize(config);
+        }
+    }
+}

src/Microsoft.AspNet.WebHooks.Receivers.Zendesk/WebHooks/ZendeskDevice.cs
@@ -1,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNet.WebHooks
+{
+    /// <summary>
+    /// Contains the information about registered device for Zendesk (iOS, Android)
+    /// </summary>
+    public class ZendeskDevice
+    {
+        /// <summary>
+        /// The device identifier/token that was registered through the SDK
+        /// </summary>
+        [JsonProperty("identifier")]
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// The device type. Possible values: "iOS" or "Android"
+        /// </summary>
+        [JsonProperty("type")]
+        public string DeviceType { get; set; }
+    }
+}