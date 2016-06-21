ac7d8168dbff31ef8de61da4c3edbef95ed227be
Michael Baker <michael@teambaker.co.nz>
2016-06-21 21:22:47 +1200

    Fixed WebHookReceiverSecret XML documentation to reflect the expected key name

--- src/Microsoft.AspNet.WebHooks.Receivers.VSTS/WebHooks/VstsWebHookReceiver.cs
+++ src/Microsoft.AspNet.WebHooks.Receivers.VSTS/WebHooks/VstsWebHookReceiver.cs
@@ -16,7 +16,7 @@ namespace Microsoft.AspNet.WebHooks
     /// 
     /// The corresponding WebHook URI is of the form '<c>https://&lt;host&gt;/api/webhooks/incoming/vsts/{id}?code={code}</c>'.
     /// For security reasons the WebHook URI must be an <c>https</c> URI and contain a 'code' query parameter with the
-    /// same value as configured in the '<c>MS_WebHookReceiverSecret_Tfs</c>' application setting, optionally using IDs
+    /// same value as configured in the '<c>MS_WebHookReceiverSecret_VSTS</c>' application setting, optionally using IDs
     /// to differentiate between multiple WebHooks, for example '<c>secret0, id1=secret1, id2=secret2</c>'.
     /// The 'code' parameter must be between 32 and 128 characters long.
     /// 
