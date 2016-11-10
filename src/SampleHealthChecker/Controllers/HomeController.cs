7eb358e68a1ea5d8f8bb1112edea085d6328bc2d
damienbod <damien_bod@hotmail.com>
2016-11-10 22:21:25 +0100

    code clean up

--- src/SampleHealthChecker/Controllers/HomeController.cs
+++ src/SampleHealthChecker/Controllers/HomeController.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using HealthChecks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SampleHealthChecker.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHealthCheckService _healthCheck;
+
+        public HomeController(IHealthCheckService healthCheck)
+        {
+            _healthCheck = healthCheck;
+        }
+
         public async Task<IActionResult> Index()
         {
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("http://localhost:5050");
-            var result = "healthy";
-
-            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            string result;
+            if (await _healthCheck.CheckHealthAsync())
+            {
+                result = "healthy";
+            }
+            else
             {
                 result = "unhealthy!";
             }
