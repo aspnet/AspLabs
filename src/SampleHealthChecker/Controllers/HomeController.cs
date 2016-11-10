8bcb6117bd6e956aa1c497060a2476bffb939aa5
damienbod <damien_bod@hotmail.com>
2016-11-10 20:02:28 +0100

    adding a sample template

---
+++ src/SampleHealthChecker/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleHealthChecker.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "yes I'm heathly";
+        }
+
+    }
+}
