f7790a1f1986ffd012ea4c58717d2a75352bc376
JuergenGutsch <juergen@gutsch-online.de>
2016-11-10 13:41:49 -0800

    Add sample web application

---
+++ src/SampleAzureServicesChecks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SampleAzureServicesChecks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseHealthChecks(8080)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
