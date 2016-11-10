c81534b74275698b768877a7982889ff29086d4f
damienbod <damien_bod@hotmail.com>
2016-11-10 21:41:24 +0100

    Showing the status of the check

--- src/SampleHealthChecker/Controllers/HomeController.cs
+++ src/SampleHealthChecker/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,19 @@ namespace SampleHealthChecker.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            ViewData["Message"] = "Your application description page.";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://localhost:5050");
+            var result = "healthy";
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                result = "unhealthy!";
+            }
+
+            ViewData["AppStatus"] = result;
+
             return View();
         }
 
