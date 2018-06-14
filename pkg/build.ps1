d8a7e17b98709e4687e0419e4b2796b393b598d7
Nate McMaster <natemcmaster@users.noreply.github.com>
2018-06-13 23:23:08 -0700

    Create a prototype of Microsoft.AspNetCore.App.RefOnly (#25)
    Prototype what it would look like for aspnetcore to follow the pattern set by Microsoft.NETCore.App. Differences between Microsoft.AspNetCore.App.RefOnly and Microsoft.AspNetCore.App
    
    * The package only contains reference assemblies in ref/netcoreapp2.1/
    * The package does not have dependencies
    * There are not implementation binaries in lib/

---
+++ pkg/build.ps1
@@ -0,0 +1,42 @@
+$ErrorActionPreference = 'stop'
+
+mkdir -p "$PSScriptRoot/obj" -ea ignore | out-null
+
+function Invoke-Block ([scriptblock] $_cmd) {
+    & $_cmd
+    if ((-not $?) -or ($LASTEXITCODE -ne 0)) {
+        throw "command failed"
+    }
+}
+
+$nuget = "$PSScriptRoot/obj/nuget.exe"
+if (-not (Test-Path $nuget)) {
+    iwr -o $nuget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+}
+
+$dotnet = "$PSScriptRoot/.dotnet/dotnet"
+$dotnetInstall = "$PSScriptRoot/obj/dotnet-install.ps1"
+if (-not (Test-Path $dotnetInstall)) {
+    iwr -o $dotnetInstall https://raw.githubusercontent.com/dotnet/cli/release/2.1.3xx/scripts/obtain/dotnet-install.ps1
+}
+& $dotnetInstall -installdir "$PSScriptRoot/.dotnet" -version 2.1.300
+Copy-Item -Recurse "$PSScriptRoot/.dotnet/shared/Microsoft.AspNetCore.App" "$PSScriptRoot/.dotnet/shared/Microsoft.AspNetCore.App.RefOnly"
+mv "$PSScriptRoot/.dotnet/shared/Microsoft.AspNetCore.App.RefOnly/2.1.0/Microsoft.AspNetCore.App.deps.json" "$PSScriptRoot/.dotnet/shared/Microsoft.AspNetCore.App.RefOnly/2.1.0/Microsoft.AspNetCore.App.RefOnly.deps.json"
+mv "$PSScriptRoot/.dotnet/shared/Microsoft.AspNetCore.App.RefOnly/2.1.0/Microsoft.AspNetCore.App.runtimeconfig.json" "$PSScriptRoot/.dotnet/shared/Microsoft.AspNetCore.App.RefOnly/2.1.0/Microsoft.AspNetCore.App.RefOnly.runtimeconfig.json"
+$refsPublish = "$PSScriptRoot/Microsoft.AspNetCore.App.RefOnly/ref/netcoreapp2.1"
+Invoke-Block { & $dotnet publish Microsoft.AspNetCore.App.RefOnly/Microsoft.AspNetCore.App.RefOnly.csproj --output $refsPublish }
+rm "$refsPublish/Microsoft.AspNetCore.App.RefOnly.dll"
+rm "$refsPublish/runtimes/" -Recurse
+rm "$refsPublish/*.json"
+Invoke-Block { & $nuget pack "$PSScriptRoot/Microsoft.AspNetCore.App.RefOnly/Microsoft.AspNetCore.App.RefOnly.nuspec" -OutputDirectory "$PSScriptRoot/feed" }
+
+rm $env:USERPROFILE/.nuget/packages/Microsoft.AspNetCore.App.RefOnly -Recurse -ea ignore
+Invoke-Block { & $dotnet publish test/test.csproj -o "$(pwd)/publish/" }
+
+pushd publish
+try {
+    Invoke-Block { & $dotnet test.dll }
+}
+finally {
+    popd
+}
