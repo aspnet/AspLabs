48b69515c975407d5bbadd11806fcf967e18cc75
Nate McMaster <nate.mcmaster@microsoft.com>
2018-10-09 12:27:04 -0700

    Remove experiments for HealthChecks
    This experiment is shipping in 2.2

--- src/HealthCheck/HealthCheckResults.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace HealthChecks
-{
-    public class HealthCheckResults
-    {
-        public IList<HealthCheckResult> CheckResults { get; } = new List<HealthCheckResult>();
-    }
-}
