1a2b2f750d152b9395e29f6a389388fb8f7b9705
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-10-17 17:19:22 -0700

    add ETW-based collector to dotnet-collect

--- src/dotnet-monitor/src/dotnet-collect/EtwCollector.cs
+++ src/dotnet-monitor/src/dotnet-collect/EtwCollector.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Session;
 
 namespace Microsoft.Diagnostics.Tools.Collect
 {
-    public class EtwCollector: EventCollector
+    public class EtwCollector : EventCollector
     {
         private readonly CollectionConfiguration _config;
+        private TraceEventSession _session;
 
         public EtwCollector(CollectionConfiguration config)
         {
@@ -14,12 +20,55 @@ namespace Microsoft.Diagnostics.Tools.Collect
 
         public override Task StartCollectingAsync()
         {
-            throw new NotImplementedException();
+            // TODO: Allow a file name to be provided
+            var outputFile = _config.ProcessId == null ?
+                Path.Combine(_config.OutputPath, "dotnet-collect.etl") :
+                Path.Combine(_config.OutputPath, $"dotnet-collect.{_config.ProcessId.Value}.etl");
+            if (File.Exists(outputFile))
+            {
+                throw new InvalidOperationException($"Target file already exists: {outputFile}");
+            }
+            _session = new TraceEventSession("dotnet-collect", outputFile);
+
+            if (_config.CircularMB is int circularMb)
+            {
+                _session.CircularBufferMB = circularMb;
+            }
+
+            var options = new TraceEventProviderOptions();
+            if (_config.ProcessId is int pid)
+            {
+                options.ProcessIDFilter = new List<int>() { pid };
+            }
+
+            // Enable the providers requested
+            foreach (var provider in _config.Providers)
+            {
+                _session.EnableProvider(provider.Provider, ConvertLevel(provider.Level), provider.Keywords, options);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private TraceEventLevel ConvertLevel(EventLevel level)
+        {
+            switch (level)
+            {
+                case EventLevel.Critical: return TraceEventLevel.Critical;
+                case EventLevel.Error: return TraceEventLevel.Error;
+                case EventLevel.Informational: return TraceEventLevel.Informational;
+                case EventLevel.LogAlways: return TraceEventLevel.Always;
+                case EventLevel.Verbose: return TraceEventLevel.Verbose;
+                case EventLevel.Warning: return TraceEventLevel.Warning;
+                default:
+                    throw new InvalidOperationException($"Unknown EventLevel: {level}");
+            }
         }
 
         public override Task StopCollectingAsync()
         {
-            throw new NotImplementedException();
+            _session.Dispose();
+            return Task.CompletedTask;
         }
     }
 }
