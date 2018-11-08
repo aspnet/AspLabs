9d0643d50120a96b5a049170557c246df05a3f74
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-11-08 15:34:36 -0800

    analyze ui prototype

---
+++ src/DotNetDiagnostics/src/DotNet.Analyzer/Pages/Runtime.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNet.Analyzer.Pages
+{
+    public class RuntimeModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
