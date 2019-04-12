3afbfa88d9bdd66e41a25a3d7a6a38b5d1ba662d
Martin Stenhoff <martin.stenhoff@team.telstra.com>
2019-04-12 18:16:54 +1000

    Fix style

--- src/Proxy/src/ProxyService.cs
+++ src/Proxy/src/ProxyService.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -21,6 +21,7 @@ namespace Microsoft.AspNetCore.Proxy
         }
 
         public SharedProxyOptions Options { get; private set; }
+
         internal HttpClient Client { get; private set; }
     }
 }
