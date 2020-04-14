8d55f18e34b6a4454233484479f4b98a8a4ccee5
James Newton-King <james@newtonking.com>
2020-04-14 21:31:36 +1200

    Add swagger to example (#272)

--- src/GrpcHttpApi/sample/Server/Startup.cs
+++ src/GrpcHttpApi/sample/Server/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Server
 {
@@ -13,6 +14,12 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpcHttpApi();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "gRPC HTTP API Example", Version = "v1" });
+            });
+            services.AddGrpcSwagger();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,6 +33,12 @@ namespace Server
             app.UseHttpsRedirection();
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "gRPC HTTP API Example V1");
+            });
 
             app.UseRouting();
 
