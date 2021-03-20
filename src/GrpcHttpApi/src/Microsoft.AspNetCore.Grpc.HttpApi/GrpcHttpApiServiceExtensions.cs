6210a1e072a4a9bf29f6834d1adfc0eb2cb7f503
James Newton-King <james@newtonking.com>
2021-03-20 13:41:58 +1300

    Clean up

--- src/GrpcHttpApi/src/Microsoft.AspNetCore.Grpc.HttpApi/GrpcHttpApiServiceExtensions.cs
+++ src/GrpcHttpApi/src/Microsoft.AspNetCore.Grpc.HttpApi/GrpcHttpApiServiceExtensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Google.Protobuf;
 using Grpc.AspNetCore.Server.Model;
 using Microsoft.AspNetCore.Grpc.HttpApi;
 using Microsoft.Extensions.DependencyInjection.Extensions;
