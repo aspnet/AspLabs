bb29db1ac73d64641ae329a0a8447e15862e7ab8
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-08-02 12:16:13 +0100

    Make the Angular sample add and remove components dynamically

--- src/BlazorCustomElements/samples/angular-app-with-blazor/src/app/app.component.ts
+++ src/BlazorCustomElements/samples/angular-app-with-blazor/src/app/app.component.ts
@@ -4,15 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
+      <h1>Welcome to {{title}}!</h1>
+      <p>This is an Angular application that can also host Blazor components.</p>
+      <p>
+        <button (click)="addBlazorCounter()">Add Blazor counter</button>
+        <button (click)="removeBlazorCounter()">Remove Blazor counter</button>
+      </p>
+
+      <div *ngFor="let blazorCounter of blazorCounters; let myIndex = index">
+        <my-counter [attr.increment-amount]="myIndex + 1"></my-counter>
+      </div>
     </div>
-    <my-counter increment-amount="2"></my-counter>
   `,
   styles: []
 })
 export class AppComponent {
   title = 'angular-app-with-blazor';
+
+  blazorCounters: any[] = [];
+
+  addBlazorCounter() {
+    this.blazorCounters.push({});
+  }
+
+  removeBlazorCounter() {
+    this.blazorCounters.pop();
+  }
 }
