f40eebfc9d5dc5efb48117d133cbf7ed809bf56e
Taylor Southwick <tasou@microsoft.com>
2022-02-07 10:35:54 -0800

    add license header

--- src/SystemWebAdapters/src/SameSiteMode.cs
+++ src/SystemWebAdapters/src/SameSiteMode.cs
@@ -1,4 +1,7 @@
-﻿namespace System.Web
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Web
 {
     public enum SameSiteMode
     {
