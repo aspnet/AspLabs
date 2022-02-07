8e54141f729de67955df8a2d9fdf661c8872094d
Taylor Southwick <tasou@microsoft.com>
2022-02-07 10:31:37 -0800

    Add additional APIs for HttpContext adapters

---
+++ src/SystemWebAdapters/src/Caching/Cache.cs
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+
+namespace System.Web.Caching
+{
+    public class Cache : IEnumerable
+    {
+        public static readonly DateTime NoAbsoluteExpiration = DateTime.MaxValue;
+
+        public static readonly TimeSpan NoSlidingExpiration = TimeSpan.Zero;
+
+        public object this[string key]
+        {
+            get => Get(key);
+            set => Insert(key, value);
+        }
+
+        public object Get(string key) => throw new NotImplementedException();
+
+        public void Insert(string key, object value) => throw new NotImplementedException();
+
+        public object Remove(string key) => throw new NotImplementedException();
+
+        public IEnumerator GetEnumerator() => throw new NotImplementedException();
+    }
+}
--- src/SystemWebAdapters/src/HttpContextBase.cs
+++ src/SystemWebAdapters/src/HttpContextBase.cs
@@ -1,6 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections;
+using System.Security.Principal;
+
 namespace System.Web
 {
     public class HttpContextBase : IServiceProvider
@@ -8,6 +11,18 @@ namespace System.Web
         public virtual HttpRequestBase Request => throw new NotImplementedException();
 
         public virtual HttpResponseBase Response => throw new NotImplementedException();
+
+        public virtual IDictionary Items => throw new NotImplementedException();
+
+        public virtual IPrincipal User
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public HttpSessionStateBase Session => throw new NotImplementedException();
+
+        public HttpServerUtilityBase Server => throw new NotImplementedException();
 
         public object GetService(Type serviceType)
         {
