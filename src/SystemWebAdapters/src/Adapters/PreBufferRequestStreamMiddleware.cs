b31b943897c5f68b5310863c495637d69ade0074
Taylor Southwick <tasou@microsoft.com>
2022-03-15 11:39:26 -0700

    Only run async if prebuffering

--- src/SystemWebAdapters/src/Adapters/PreBufferRequestStreamMiddleware.cs
+++ src/SystemWebAdapters/src/Adapters/PreBufferRequestStreamMiddleware.cs
@@ -18,26 +18,33 @@ internal class PreBufferRequestStreamMiddleware : IMiddleware
         _logger = logger;
     }
 
-    public async Task InvokeAsync(HttpContextCore context, RequestDelegate next)
+    public Task InvokeAsync(HttpContextCore context, RequestDelegate next)
     {
         if (context.GetEndpoint()?.Metadata.GetMetadata<IPreBufferRequestStreamMetadata>() is { IsEnabled: true } metadata)
         {
-            var feature = context.Features.Get<IHttpMaxRequestBodySizeFeature>();
-            var bufferLimit = (feature?.MaxRequestBodySize, metadata.BufferLimit) switch
-            {
-                (null, long limit) => limit, 
-                (long limit, null) => limit,
-                (long featureLimit, long metadataLimit) => Math.Min(featureLimit, metadataLimit),
-                _ => long.MaxValue,
-            };
-
-            _logger.LogTrace("Buffering request stream");
-
-            context.Request.EnableBuffering(metadata.BufferThreshold, bufferLimit);
-
-            await context.Request.Body.DrainAsync(context.RequestAborted);
-            context.Request.Body.Position = 0;
+            return PreBufferAsync(context, metadata, next);
         }
+
+        return next(context);
+    }
+
+    private async Task PreBufferAsync(HttpContextCore context, IPreBufferRequestStreamMetadata metadata, RequestDelegate next)
+    {
+        var feature = context.Features.Get<IHttpMaxRequestBodySizeFeature>();
+        var bufferLimit = (feature?.MaxRequestBodySize, metadata.BufferLimit) switch
+        {
+            (null, long limit) => limit,
+            (long limit, null) => limit,
+            (long featureLimit, long metadataLimit) => Math.Min(featureLimit, metadataLimit),
+            _ => long.MaxValue,
+        };
+
+        _logger.LogTrace("Buffering request stream");
+
+        context.Request.EnableBuffering(metadata.BufferThreshold, bufferLimit);
+
+        await context.Request.Body.DrainAsync(context.RequestAborted);
+        context.Request.Body.Position = 0;
 
         await next(context);
     }
