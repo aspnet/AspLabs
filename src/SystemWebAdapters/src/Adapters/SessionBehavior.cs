a7ad21a4a4dbe031d938c90dc443cf89781d1195
Taylor Southwick <tasou@microsoft.com>
2022-04-16 08:20:14 -0700

    Rename to SessionBehavior.PreLoad (#490)

--- src/SystemWebAdapters/src/Adapters/SessionBehavior.cs
+++ src/SystemWebAdapters/src/Adapters/SessionBehavior.cs
@@ -5,9 +5,19 @@ namespace System.Web.Adapters;
 
 public enum SessionBehavior
 {
+    /// <summary>
+    /// No session will be available on the endpoint.
+    /// </summary>
     None,
-    Eager,
 
-    [Obsolete("This will enable session on the endpoint but will resort to async over sync behavior")]
+    /// <summary>
+    /// Asynchronously loads the session for controllers with this attribute before running the controller.
+    /// </summary>
+    PreLoad,
+
+    /// <summary>
+    /// Synchronously loads the session for controllers with this attribute on first use.
+    /// </summary>
+    [Obsolete("This will enable session on the endpoint but will resort to sync over async behavior")]
     OnDemand,
 }
