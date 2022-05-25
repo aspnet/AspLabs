2c4370cb7bdedde9161f72a6786b46d2b7fa5e4e
Taylor Southwick <tasou@microsoft.com>
2022-05-25 13:14:53 -0700

    Remove systemweb-adapters
    Future work will now be done at: https://github.com/dotnet/systemweb-adapters

--- src/SystemWebAdapters/samples/MvcCoreApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcCoreApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- src/SystemWebAdapters/test/Microsoft.AspNetCore.SystemWebAdapters.SessionState.Tests/IsExternalInit.cs
+++
@@ -1,5 +0,0 @@
-namespace System.Runtime.CompilerServices;
-
-internal static class IsExternalInit
-{
-}
