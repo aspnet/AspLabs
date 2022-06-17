56daec5cacb95c62313356fd67222505c304486e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2022-06-17 17:26:54 +0100

    License headers

--- src/QuickGrid/src/Microsoft.AspNetCore.Components.QuickGrid/Infrastructure/InternalGridContext.cs
+++ src/QuickGrid/src/Microsoft.AspNetCore.Components.QuickGrid/Infrastructure/InternalGridContext.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 namespace Microsoft.AspNetCore.Components.QuickGrid.Infrastructure;
 
 // The grid cascades this so that descendant columns can talk back to it. It's an internal type
