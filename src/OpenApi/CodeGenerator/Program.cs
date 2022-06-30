4798f3575976b2a4adea5187aaf3f8ef94cfcfbb
BArthur1972 <82739341+BArthur1972@users.noreply.github.com>
2022-06-30 12:33:51 -0700

    Clean Up

--- src/OpenApi/CodeGenerator/Program.cs
+++ src/OpenApi/CodeGenerator/Program.cs
@@ -3,19 +3,10 @@ using Microsoft.OpenApi.Readers;
 
 class App
 {
-    //Things to read in from the JSON: 
-    //Info (which includes the version and the Title)
-    //Servers
-    //Path
-    //For each path we need to also read in the operations
-    //For each operation we need to read in the Descriptions and Responses.
-
     public static void Main(string[] args)
     {
         OpenApiDiagnostic diagnostic = new OpenApiDiagnostic();
         var reader = new OpenApiStreamReader();
-
-        //var path = "C:\\Users\\t-barthur\\Downloads\\openapi.json";
 
         Stream stream = File.OpenRead(args[0]);
         var newDocument = reader.Read(stream, out diagnostic);
