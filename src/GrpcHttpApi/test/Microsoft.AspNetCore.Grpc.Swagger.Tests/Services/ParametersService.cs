a59600ca57eb854b87b93fb8bb7551ef99b92e61
Medvedev Nikolay Yuryevich <nikomedvedev@ozon.ru>
2022-08-23 20:26:06 +0300

    [query] GrpcHttpApi: add test for query parameter resolver

---
+++ src/GrpcHttpApi/test/Microsoft.AspNetCore.Grpc.Swagger.Tests/Services/ParametersService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Params;
+
+namespace Microsoft.AspNetCore.Grpc.Swagger.Tests.Services;
+
+public class ParametersService : Parameters.ParametersBase
+{
+    public override Task<Response> DemoParametersOne(RequestOne requestId, ServerCallContext ctx)
+    {
+        return Task.FromResult(new Response {Message = "DemoParametersOne Response"});
+    }
+    
+    public override Task<Response> DemoParametersTwo(RequestOne requestId, ServerCallContext ctx)
+    {
+        return Task.FromResult(new Response {Message = "DemoParametersTwo Response"});
+    }
+
+    public override Task<Response> DemoParametersThree(RequestTwo request, ServerCallContext ctx)
+    {
+        return Task.FromResult(new Response {Message = "DemoParametersThree Response "});
+    }
+    
+    public override Task<Response> DemoParametersFour(RequestTwo request, ServerCallContext ctx)
+    {
+        return Task.FromResult(new Response {Message = "DemoParametersFour Response"});
+    }
+}
